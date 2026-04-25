61412aa44b02fb4fd6c97948f9f42e476b2ebc00
Reparadoxo <joaofe.mokdse@gmail.com>
2026-04-25 19:53:38 -0300

    Fixing version

--- Crash/obj/Debug/net8.0/Crash.AssemblyInfo.cs
+++ Crash/obj/Debug/net8.0/Crash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39bd728903cae2024b3b805c22d3baae83ad939c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce398160a95620e1d75f912ef216a0c15dc9bd8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
